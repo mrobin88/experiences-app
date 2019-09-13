7a38237fb324d01fc9faad7d20644fb5fe271735
mrobin88 <ratthewrobin@gmail.com>
2019-09-13 10:24:32 -0700

    Adds Booking, Review, and City, to models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    experience = models.ForeignKey(Experience, on_delete=models.CASCADE)
+
+class Review(models.Model):
+    experience = models.ForeignKey(Experience, on_delete=models.CASCADE)
+    rating = models.IntegerField(min=1, max=5)
+    comment = models.TextField(max_length=250)
+
+class City:
+    location = models.CharField(max_length=60)
+
+
+    
+
+
+
