af198191dddff4cd43f67cce893038bd01de55b4
Greg <gmerrill03@gmail.com>
2019-09-13 10:17:54 -0700

    add Experience model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
+from languages.fields import LanguageField
 
-# Create your models here.
+
+class Experience(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    location = models.CharField(max_length=100)
+    hours = models.IntegerField(validators=[MaxValueValidator(24)])
+    minutes = models.IntegerField(validators=[MaxValueValidator(60)])
+    language = LanguageField()
+    # user in this case is equal to the experience host
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
