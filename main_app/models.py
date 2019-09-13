d96c9fa69212628351b132e664142d90e090b058
Greg <gmerrill03@gmail.com>
2019-09-13 15:24:49 -0700

    organize import code lines at top of pages

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.conf import settings
 from django.urls import reverse
-from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 from django.core.validators import MaxValueValidator, MinValueValidator
+
 from PIL import Image
 from datetime import date
 from languages.fields import LanguageField
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, redirect
 from django.contrib.auth import login 
 from django.views.generic import DetailView
-from django.views.generic.edit import CreateView, DeleteView
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.views.generic.edit import CreateView, DeleteView
+
 from .models import Experience
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def home(request):
     return render(request, 'home.html')
