ed5f954a22c8ab75489b55e16a50ec373ff7644b
Greg <gmerrill03@gmail.com>
2019-09-13 13:39:08 -0700

    add create experience routes and form page

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login 
-from django .contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from .models import Experience
 
 def home(request):
     return render(request, 'home.html')
@@ -41,3 +44,8 @@ def profile(request):
         'p_form': p_form
     }
     return render(request, 'registration/profile.html', context)
+
+class ExperienceCreate(LoginRequiredMixin, CreateView):
+    model = Experience
+    fields = ['title', 'description', 'price', 'location', 'hours', 'minutes', 'language']
+    template_name = 'experiences/form.html'
