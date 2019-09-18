183dd21172f61b838f34d5c7ddac945fe3c4a972
g-merrill <44042970+g-merrill@users.noreply.github.com>
2019-09-18 11:35:43 -0700

    Update urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,6 @@
-  
 from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
